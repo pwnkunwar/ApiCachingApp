9a8f74178b3b19d45796d27b714a3559be35219c
pwn1kunwar <pwn1kunwar@gmail.com>
2024-09-14 21:44:30 +0545

    Initial Commit

---
+++ ApiCachingApp/Controllers/DriverController.cs
@@ -0,0 +1,43 @@
+﻿using ApiCachingApp.Data;
+using ApiCachingApp.Models;
+using ApiCachingApp.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Runtime.InteropServices;
+
+namespace ApiCachingApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DriverController :ControllerBase
+    {
+        private readonly ICacheService _cacheService;
+        private readonly ApiDbContext _dbContext;
+        public DriverController(ICacheService cacheService, ApiDbContext dbContext)
+        {
+            _cacheService = cacheService;
+            _dbContext = dbContext;
+        }
+
+        [HttpGet("drivers")]
+        public async Task<IActionResult> Get()
+        {
+            var cacheDrivers = _cacheService.GetData<IEnumerable<Driver>>("drivers");
+            if(cacheDrivers != null && cacheDrivers.Count() > 0)
+            {
+                return Ok(cacheDrivers);
+            }
+            var drivers = await _dbContext.Drivers.ToListAsync();
+            var expireTime = DateTimeOffset.Now.AddMinutes(2);
+            _cacheService.SetData<IEnumerable<Driver>>("drivers", drivers, expireTime);
+            return Ok(drivers);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Post(Driver driver)
+        {
+            await _dbContext.Drivers.AddAsync(driver);
+            await _dbContext.SaveChangesAsync();
+            return Ok(driver);
+        }
+    }
+}
---
+++ ApiCachingApp/Data/ApiDbContext.cs
@@ -0,0 +1,15 @@
+﻿using ApiCachingApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiCachingApp.Data
+{
+    public class ApiDbContext : DbContext
+    {
+        public DbSet<Driver> Drivers { get; set; }
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) 
+            : base(options)
+        {
+            
+        }
+    }
+}
